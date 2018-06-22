83d8206456262e4cf2d270faaeda6dc588f23995
Charles Reusnow <Charles.J.Reusnow@centene.com>
2018-06-21 21:13:11 -0500

    FIRST WORKING INSTANCE OF GRPC WEB CLIENT
    Added in the stupid http and https requirements to package.json which didn't even fix the grpc-web-client issue, so I had to make 2 empty files so node can resolve the module that it will never use.
    
    Added in test button to show that unary calls are working to the back end.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {Evolve, EvolveRequest, EvolveResponse} from "./pb/evolve_pb";
+import {EvolveService} from "./pb/evolve_pb_service";
+import {grpc} from "grpc-web-client";
 
 @Component({
   selector: "evolve-ui-root",
@@ -15,5 +18,20 @@ export class AppComponent implements OnInit {
 
   public titleClicked(): void {
     this.router.navigate(["/evolve", {}]);
+  }
+
+  evolve(): void {
+    const evolveRequest: EvolveRequest = new EvolveRequest();
+    const evolve: Evolve = new Evolve();
+    evolve.setA(55);
+    evolveRequest.setEvolve(evolve);
+
+    grpc.unary(EvolveService.Evolve, {
+      request: evolveRequest,
+      host: "http://localhost:8080",
+      onEnd: (response: any): void => {
+        console.log(response.message.toObject());
+      }
+    });
   }
 }
